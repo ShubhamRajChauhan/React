c3b4eeb2286138a9677a0b8476706e07d43379bc
Shubham <shubhamrajchauhan07@gmail.com>
2025-02-03 12:33:49 +0530

    react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,11 +3,12 @@ import Chai from "./chai"
 
 
 function App() {
+  const username = "chai aur code"
 
   return (
     <>
     <Chai/>
-    <h1>chai aur react with vite</h1>
+     <h1>chai aur react with vite {username}</h1>  {/*  {}==> ishko evaluated expression kehte hai , kyun ki ishke under jo evaluated or final outcome aati hai java script ki wo pass karte hai */}
     <p>test para</p>
     </>
     
